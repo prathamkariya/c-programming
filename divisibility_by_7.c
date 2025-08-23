2f9f26f176b359c02d1fb97c023fc9fb2031b3de
prathamkariya <pratham.kariya1@gmail.com>
2025-08-23 14:12:37 +0530

    Added net sales program and updated divisibility check

--- divisibility_by_7.c
+++ divisibility_by_7.c
@@ -2,7 +2,7 @@
 int main() {
     int n;
     printf("Enter a number: \n");
-    scanf_s("%d", &n);
+    scanf("%d", &n);
     if(n%7==0) printf("Divisible by 7\n");
     else printf("Not divisible by 7\n");
     return 0;
