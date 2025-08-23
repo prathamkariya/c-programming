e2a2720e148f080b501057316b1175e66a3ba999
prathamkariya <pratham.kariya1@gmail.com>
2025-08-23 14:18:47 +0530

    git commit -m "Added net sales"

---
+++ net_sales_using_if_else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    float sales,disc,net;
+    printf("Enter sales amount: ");
+    scanf("%f",&sales);
+    if(sales>20000) 
+        disc=0.15*sales;
+    else if(sales>10000) 
+        disc=0.1*sales;
+    else 
+        disc=0.05*sales;
+    net=sales-disc;
+    
+    printf("Net sales=%.2f\n",net);
+    return 0;
+}
+
+
