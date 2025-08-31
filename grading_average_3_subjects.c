8058fbd8e27b5e053ea69591d100b93b9195e744
prathamkariya <pratham.kariya1@gmail.com>
2025-08-31 12:25:35 +0530

    grading avg of 3 subjects new

--- grading_average_3_subjects.c
+++ grading_average_3_subjects.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 int main(){
-    int s1,s2,s3;
+    int s1,s2,s3,total;
     float avg;
     printf("Enter marks of three subjects: ");
     scanf("%d%d%d",&s1,&s2,&s3);
-    avg=(s1+s2+s3)/3.0;
+    total=s1+s2+s3;
+    avg=(total)/3.0;
+    printf("Total Marks: %d\n",total);
+    printf("Average Marks: %.2f\n",avg);
     if(s1<35||s2<35||s3<35)
         printf("Failed\n");
     else if(avg>=70)
