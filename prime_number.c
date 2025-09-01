8bcecf27440bdfb40ec4c3249f52fa479fb6964a
Pratham Kariya <pratham.kariya1@gmail.com>
2025-09-01 10:12:06 +0530

    Add files via upload

---
+++ prime_number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    if(n<2){
+        printf("%d is Not Prime\n",n);
+        retunr 0;
+
+    }
+
+    for(i=2;i*i<=n;i++){
+        if(n%i==0){
+            printf("%d is Not Prime\n",n);
+            return 0;
+        }
+    }
+
+    printf("%d is Prime\n",n);
+    return 0;
+}
