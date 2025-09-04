195fc906266c0bb39ef7c2d83bde08465a8c9cdb
prathamkariya <pratham.kariya1@gmail.com>
2025-09-04 22:06:44 +0530

    changes made

--- check_prime_q59.c
+++ check_prime_q59.c
@@ -1,14 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n,i;
+int main() {
+    int n, i;
     printf("Enter a number: ");
     scanf("%d",&n);
 
-    if(n<2){ // numbers <2 are not prime
+    if (n<2) {
         printf("%d is Not Prime\n",n);
         return 0;
     }
+
+    if (n==2) {   // 2 is the only even prime
+        printf("%d is Prime\n",n);
+        return 0;
+    }
+
+    if (n%2==0) { // other evens are not prime
+        printf("%d is Not Prime\n",n);
+        return 0;
+    }
+
+    // Check odd divisors only, from 3 up to √n
+    for (i=3;i*i<=n;i+=2) {
+        if (n%i==0) {
+            printf("%d is Not Prime\n",n);
+            return 0;
+        }
+    }
+
+    printf("%d is Prime\n", n);
+    return 0;
+}
 
     /*
     Prime checking optimizations:
@@ -18,14 +40,3 @@ int main(){
        - If a>√n → b<√n, If b>√n → a<√n
     2. Skip even numbers after 2: only check odd divisors
     */
-
-    for(i=2;i*i<=n;i+=1){ // check only odd divisors up to √n
-        if(n%i==0){
-            printf("%d is Not Prime\n",n);
-            return 0;
-        }
-    }
-
-    printf("%d is Prime\n",n);
-    return 0;
-}
